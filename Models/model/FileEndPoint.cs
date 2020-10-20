a010d671ada50b8e16fb6b1788251d9380553930
Nikolai Køhler Lundberg Hansen <niko9946@edu.easj.dk>
2020-10-20 09:35:59 +0200

    Library og rest solution

---
+++ Models/model/FileEndPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.model
+{
+    //Husk klasser skal være public, ellers kan man ikke refferere til den
+    public class FileEndPoint
+    {
+        #region instance fields
+
+        private string IpAddress { get; set; }
+
+        #endregion
+
+        #region properties
+
+        
+
+        #endregion
+
+
+
+    }
+}
